dd823022e5595f4193cef0cea247845d1295c6f8
tamasiadam <tamasi.adam.attila@diak.szbi-pg.hu>
2024-12-09 08:57:07 +0100

    Create lotto.py

---
+++ lotto.py
@@ -0,0 +1,26 @@
+import random
+
+print("Üdvözöllek a játékban!")
+
+jatekos = []
+generalt_szamok = [random.randint(0, 91) for i in range(5)]
+
+while len(jatekos) < 5:
+    jatekos_szamok = int(input("Adj meg egy számot! "))    
+    if jatekos_szamok >= 1 and jatekos_szamok <= 90:
+        jatekos.append(jatekos_szamok)
+    else:   
+        print("Ez nem jóÓ!!")
+
+talalat = [0]
+
+for szam in talalat :
+    if jatekos in generalt_szamok:
+        talalat += 1
+
+print(f"A te számaid: {jatekos}")
+print(f"Nyerőszámok: {generalt_szamok}")
+print(f"Találat: {talalat}")
+
+
+
