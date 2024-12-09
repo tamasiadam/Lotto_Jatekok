14b442e39935a4265a85ca9963b6b27258afa010
tamasiadam <tamasi.adam.attila@diak.szbi-pg.hu>
2024-12-09 09:24:39 +0100

    Update lotto.py

--- lotto.py
+++ lotto.py
@@ -8,19 +8,21 @@ generalt_szamok = {random.randint(1, 10) for i in range(5)}
 
 
 while len(jatekos) < 5:
-    jatekos_szamok = int(input("Adj meg egy számot! "))    
+    jatekos_szamok = int(input("Adj meg egy számot! "))
     if jatekos_szamok in jatekos:
-        print("Te butus! Ez má' vót'")
+        print("Ez a szám már volt! Válassz újat!")
     elif jatekos_szamok >= 1 and jatekos_szamok <= 90:
         jatekos.append(jatekos_szamok)
     else:   
-        print("Ez nem jóÓ!!")
+        print("Helytelen szám! Válassz újat!")
 
-talalat = 0
+# talalat = 0
+# for szam in jatekos :
+#      if jatekos_szamok in generalt_szamok:
+#         talalat += 1
 
-for szam in talalat :
-    if jatekos in generalt_szamok:
-        talalat += 1
+talalat = generalt_szamok.intersection(jatekos)
+print(f"Találatok: {len(talalat)}")
 
 print(f"A te számaid: {jatekos}")
 print(f"Nyerőszámok: {generalt_szamok}")
