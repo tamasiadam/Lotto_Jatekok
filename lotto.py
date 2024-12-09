bc5a09daa2c7834c45fd0f4cb56fe5d88722cdf6
tamasiadam <tamasi.adam.attila@diak.szbi-pg.hu>
2024-12-09 09:08:21 +0100

    majdnem jo

--- lotto.py
+++ lotto.py
@@ -3,11 +3,13 @@ import random
 print("Üdvözöllek a játékban!")
 
 jatekos = []
-generalt_szamok = [random.randint(0, 91) for i in range(5)]
+generalt_szamok = [random.randint(1, 10) for i in range(5)]
 
 while len(jatekos) < 5:
     jatekos_szamok = int(input("Adj meg egy számot! "))    
-    if jatekos_szamok >= 1 and jatekos_szamok <= 90:
+    if jatekos_szamok in jatekos:
+        print("Te butus! Ez má' vót'")
+    elif jatekos_szamok >= 1 and jatekos_szamok <= 90:
         jatekos.append(jatekos_szamok)
     else:   
         print("Ez nem jóÓ!!")
@@ -16,11 +18,13 @@ talalat = [0]
 
 for szam in talalat :
     if jatekos in generalt_szamok:
-        talalat += 1
+        talalat.append(szam)
 
 print(f"A te számaid: {jatekos}")
 print(f"Nyerőszámok: {generalt_szamok}")
 print(f"Találat: {talalat}")
 
 
-
+#Problémák:
+#nyerőszámok ne lehessen ugyanaz
+#találat számláló
